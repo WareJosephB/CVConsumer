038c248b83ab47733c0031f4191a8281f90310d5
WareJosephB <warejosephb@gmail.com>
2018-12-06 11:04:55 +0000

    Succesful test of queue connection- not sure what other tests can be added to this

--- src/main/java/com/qa/CVConsumer/util/CVProducer.java
+++ src/main/java/com/qa/CVConsumer/util/CVProducer.java
@@ -21,4 +21,5 @@ public class CVProducer {
 		jmsTemplate.convertAndSend("${outgoingQueue}", cvs);
 		return "${successful.message}";
 	}
+
 }
--- src/test/java/com/qa/CVConsumer/CvConsumerApplicationTests.java
+++ src/test/java/com/qa/CVConsumer/CvConsumerApplicationTests.java
@@ -1,16 +1,24 @@
 package com.qa.CVConsumer;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CvConsumerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
+	@Test
+	public void testSendReceive() { //Checks whether correctly pointing to MQ Server and therefore requires that an MQ server is running
+		jmsTemplate.convertAndSend("testQueue", "Hello World!");
+		assertEquals(jmsTemplate.receiveAndConvert("testQueue"), "Hello World!");
+	}
 }
